6fdc9ded3aa3f3eabe3de1385370d48a81f2542c
Kotelevsky Dmitry <kotelevskiydm@gmail.com>
2011-09-24 17:10:37 +0400

    access to the server check. Dispatched XML code

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -6,6 +6,7 @@
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -22,19 +23,59 @@ public class ServerThread extends Thread {
     private PrintWriter out;
     private int client;
     private ServerGame Game;
+    private final String POLICY_REQUEST = "<policy-file-request/>";
+    private  String POLICY_XML =
+            "<?xml version=\"1.0\"?>"
+            + "<cross-domain-policy>"
+            + "<allow-access-from domain=\"*\" to-ports=\"*\" />"
+            + "</cross-domain-policy>";
     
     public ServerThread(Socket socket, ServerGame Game) throws IOException{
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(this.socket
                 .getOutputStream())), true);
+        
+        readPolicyRequest();
+        
         client = Game.addClient(new ClientSocket(socket, out));
         this.Game = Game;
         //client = ServerGame.addClient(new ClientSocket(socket, out));        
         start();
     }
+
+    protected void readPolicyRequest() {
+           try {
+               System.out.println("OK2");
+                String request = read();
+                System.out.println("OK1");
+                System.out.println("client says '" + request + "'");
+                System.out.println(request.equals(POLICY_REQUEST));
+                if (request.equals(POLICY_REQUEST)) {
+                    System.out.println("OK");
+                   writePolicy();
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Exception (readPolicyRequest): " + e.getMessage());
+            }
+            
+    }
+    
+    protected void writePolicy() {
+        try {
+            this.out.println(POLICY_XML + "\u0000");
+            this.out.close();
+            System.out.println("policy sent to client");
+        }
+        catch (Exception e) {
+            System.out.println("Exception (writePolicy): " + e.getMessage());
+        }
+    }
+
     public void run(){
-        try{
+        try{            
+                        
             while(true){
                 String command = in.readLine();
                 
@@ -61,4 +102,28 @@ public class ServerThread extends Thread {
         }
     }
     
+    protected String read() {
+        StringBuffer buffer = new StringBuffer();
+        int codePoint;
+        boolean zeroByteRead = false;
+        
+        try {
+            do {
+                codePoint = this.in.read();
+
+                if (codePoint == 0) {
+                    zeroByteRead = true;
+                }
+                else if (Character.isValidCodePoint(codePoint)) {
+                    buffer.appendCodePoint(codePoint);
+                }
+            }
+            while (!zeroByteRead && buffer.length() < 200);
+        }
+        catch (Exception e) {
+            System.out.println("Exception (read): " + e.getMessage());
+        }
+        
+        return buffer.toString();
+    } 
 }
